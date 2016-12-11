7d58f20ed2466446704df0e03db73b840d9c159c
Joe Bowen <joe.b.bowen@gmail.com>
2016-12-11 06:24:49 -0700

    Added new image galleries for new events.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,48 @@ var es = require('event-stream');
 
 gulp.task('default', function () {
   return es.concat(
+    gulp.src('src-images/events/12-4-16-CRASH/*.jpg')
+		.pipe(responsive({
+		  '*.jpg': {
+		    width: 1100,
+		    height: 733,
+		    max: true,
+		    rotate: true,
+		  },
+		  '*': {
+		    width: 160,
+		    height: 107,
+		    max: true,
+		    rotate: true,
+		    rename: { suffix: '-thumbnail' },
+		  },
+		}, {
+		  quality: 70,
+		  progressive: true,
+		  withMetadata: false,
+		}))
+		.pipe(gulp.dest('web/images/events/12-4-16-CRASH')),
+    gulp.src('src-images/events/11-6-16-CRASH/*.jpg')
+		.pipe(responsive({
+		  '*.jpg': {
+		    width: 1100,
+		    height: 733,
+		    max: true,
+		    rotate: true,
+		  },
+		  '*': {
+		    width: 160,
+		    height: 107,
+		    max: true,
+		    rotate: true,
+		    rename: { suffix: '-thumbnail' },
+		  },
+		}, {
+		  quality: 70,
+		  progressive: true,
+		  withMetadata: false,
+		}))
+		.pipe(gulp.dest('web/images/events/11-6-16-CRASH')),
   	gulp.src('src-images/events/10-15-16-CRASH/*.jpg')
 		.pipe(responsive({
 		  '*.jpg': {
