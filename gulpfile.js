7a0448c2ab7a08106b2082daeaec4d69b43cd92e
Joe Bowen <joe.b.bowen@gmail.com>
2017-05-21 18:54:01 -0600

    Updated website with new pictures.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,48 @@ var es = require('event-stream');
 
 gulp.task('default', function () {
   return es.concat(
+    gulp.src('src-images/events/4-23-17-HARTSEL/*.jpg')
+		.pipe(responsive({
+		  '*.jpg': {
+		    width: 1100,
+		    height: 733,
+		    max: true,
+		    rotate: true,
+		  },
+		  '*': {
+		    width: 160,
+		    height: 107,
+		    max: true,
+		    rotate: true,
+		    rename: { suffix: '-thumbnail' },
+		  },
+		}, {
+		  quality: 70,
+		  progressive: true,
+		  withMetadata: false,
+		}))
+		.pipe(gulp.dest('web/images/events/4-23-17-HARTSEL')),
+    gulp.src('src-images/events/4-15-17-CRASH/*.jpg')
+		.pipe(responsive({
+		  '*.jpg': {
+		    width: 1100,
+		    height: 733,
+		    max: true,
+		    rotate: true,
+		  },
+		  '*': {
+		    width: 160,
+		    height: 107,
+		    max: true,
+		    rotate: true,
+		    rename: { suffix: '-thumbnail' },
+		  },
+		}, {
+		  quality: 70,
+		  progressive: true,
+		  withMetadata: false,
+		}))
+		.pipe(gulp.dest('web/images/events/4-15-17-CRASH')),
     gulp.src('src-images/events/1-21-17-CRASH/*.jpg')
 		.pipe(responsive({
 		  '*.jpg': {
